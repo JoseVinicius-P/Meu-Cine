bf04091413cb6b10db1cccf345f805502cd0205f
JoseVinicius-P <josevpf123@gmail.com>
2023-02-14 21:38:20 -0300

    style: added logo from movie database and redirect link to your website

--- app/src/main/java/com/jv/meusfilmes/activitys/LoginActivity.java
+++ app/src/main/java/com/jv/meusfilmes/activitys/LoginActivity.java
@@ -3,31 +3,64 @@ package com.jv.meusfilmes.activitys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.firebase.auth.FirebaseAuth;
 import com.jv.meusfilmes.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ImageView iv_logo, iv_logo_tmdb;
+    private boolean is_redirecionado = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        inicializarComponentes();
+        inicializarListeners();
+        delay();
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                abrirTelaMeusFilmes();
-            }
-        }, 1800);
+    }
 
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        is_redirecionado = false;
+        delay();
+    }
+
+    private void delay(){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if(!is_redirecionado){
+                        abrirTelaMeusFilmes();
+                    }
+                }
+            }, 1800);
+    }
+
+    private void inicializarComponentes(){
+        iv_logo = findViewById(R.id.iv_logo);
+        iv_logo_tmdb = findViewById(R.id.iv_logo_tmdb);
+    }
+
+    private void inicializarListeners(){
+        iv_logo_tmdb.setOnClickListener(v -> {
+            redirecionarUsuario();
+        });
+    }
+
+    private void redirecionarUsuario(){
+        String url = "https://www.themoviedb.org";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+        is_redirecionado = true;
     }
 
     public void abrirTelaMeusFilmes(){
