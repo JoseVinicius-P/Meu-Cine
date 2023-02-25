493d2240fb43eaefe7f843dd9a0e04f11a780944
JoseVinicius-P <josevpf123@gmail.com>
2023-02-25 09:56:36 -0300

    refactor: removed SharedPreferences and added references to the FilmDAO class in this activity

--- app/src/main/java/com/jv/meusfilmes/activitys/MeusFilmesActivity.java
+++ app/src/main/java/com/jv/meusfilmes/activitys/MeusFilmesActivity.java
@@ -16,6 +16,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.jv.meusfilmes.R;
 import com.jv.meusfilmes.adapters.AdapterFilmes;
 import com.jv.meusfilmes.api.TmdbFilme;
+import com.jv.meusfilmes.dao.FilmeDAO;
 import com.jv.meusfilmes.models.Filme;
 import com.jv.meusfilmes.utilities.RecyclerItemClickListener;
 
@@ -26,8 +27,8 @@ public class MeusFilmesActivity extends AppCompatActivity {
 
     //Componentes da tela
     private FloatingActionButton fab_add_filme;
-    private SharedPreferences shared_preferences_filmes;
-    private SharedPreferences.Editor editor;
+
+    private FilmeDAO filme_dao;
     private RecyclerView rv_meus_filmes;
     private AdapterFilmes adapter_filmes;
     private RecyclerView.LayoutManager layoutManager;
@@ -41,19 +42,18 @@ public class MeusFilmesActivity extends AppCompatActivity {
 
         inicializarComponentes();
         addListeners();
-        buscarMeusFilmes(getIdsFilmes());
+        buscarMeusFilmes(filme_dao.getIdsFilmes());
     }
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
-        buscarMeusFilmes(getIdsFilmes());
+        buscarMeusFilmes(filme_dao.getIdsFilmes());
     }
 
     private void inicializarComponentes(){
         fab_add_filme = findViewById(R.id.fab_add_filme);
-        shared_preferences_filmes = getSharedPreferences("MinhaListaFilmes", MODE_PRIVATE);
-        editor = shared_preferences_filmes.edit();
+        filme_dao = new FilmeDAO(this);
         rv_meus_filmes = findViewById(R.id.rv_meus_filmes);
         //Criando mananger para o recycler view
         layoutManager = new GridLayoutManager(getApplicationContext(), 2);
@@ -78,7 +78,7 @@ public class MeusFilmesActivity extends AppCompatActivity {
 
                             @Override
                             public void onLongItemClick(View view, int position) {
-                                apagarFilmeLista(position);
+                                filme_dao.apagarFilmeLista(adapter_filmes.getIdFilme(position));
                                 adapter_filmes.removerFilme(position);
                             }
 
@@ -97,59 +97,9 @@ public class MeusFilmesActivity extends AppCompatActivity {
         startActivity(detalhe_filme);
     }
 
-    private void apagarFilmeLista(int position){
-        List<Integer> ids_filmes_local = getIdsFilmes();
-        int id_filme = adapter_filmes.getIdFilme(position);
-        if(ids_filmes_local != null && ids_filmes_local.size() > 0){
-            for (int i = 0; i < ids_filmes_local.size(); i++) {
-                if(id_filme == ids_filmes_local.get(i)){
-                    ids_filmes_local.remove(i);
-                }
-            }
-            editor.clear();
-            if (editor.commit()){
-                salvarListaFilmes(ids_filmes_local);
-            }
-        }
-    }
-
-    private void salvarListaFilmes(List<Integer> ids_filmes){
-        if(ids_filmes.size() > 0){
-            for (int i = 1; i <= ids_filmes.size(); i++) {
-                String chave_filme = "filme_" + i;
-                editor.putInt(chave_filme, ids_filmes.get(i-1));
-            }
-            editor.apply();
-        }else{
-            rv_meus_filmes.setVisibility(View.GONE);
-            tv_sem_filmes.setVisibility(View.VISIBLE);
-        }
-
-    }
-
     private void abrirTelaListaFilmes(){
         Intent lista_filmes = new Intent(this, ListaFilmesActivity.class);
         startActivity(lista_filmes);
-    }
-
-    private List<Integer> getIdsFilmes(){
-        List<Integer> ids_filmes = new ArrayList<>();
-        int qt_filmes = shared_preferences_filmes.getAll().size();
-        //Se lista de filmes estiver vazia uma mensagem será exibida
-        if (qt_filmes > 0){
-            for(int i = 1; i <= qt_filmes; i++){
-                String chave_filme = "filme_" + i;
-                ids_filmes.add(shared_preferences_filmes.getInt(chave_filme, 0));
-            }
-        }else {
-            tv_sem_filmes.setVisibility(View.VISIBLE);
-        }
-
-        //Retonará nulo para fazer tratamento no outro metodo
-        if (ids_filmes.size() > 0)
-            return ids_filmes;
-        else
-            return null;
     }
 
     private void buscarMeusFilmes(List<Integer> ids_filmes){
