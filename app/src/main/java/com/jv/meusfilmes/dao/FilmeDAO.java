3bed9ba2425aa72af0d020c3da916be5715e85cb
JoseVinicius-P <josevpf123@gmail.com>
2023-02-25 09:43:24 -0300

    feat: add packade dao and filmeDAO class

---
+++ app/src/main/java/com/jv/meusfilmes/dao/FilmeDAO.java
@@ -0,0 +1,66 @@
+package com.jv.meusfilmes.dao;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmeDAO {
+    private SharedPreferences shared_preferences_filmes;
+    private SharedPreferences.Editor editor;
+
+    public FilmeDAO(Context context) {
+        this.shared_preferences_filmes = context.getSharedPreferences("MinhaListaFilmes", Context.MODE_PRIVATE);
+        this.editor = shared_preferences_filmes.edit();
+    }
+
+    public void apagarFilmeLista(int id_filme){
+        List<Integer> ids_filmes_local = getIdsFilmes();
+        if(ids_filmes_local != null && ids_filmes_local.size() > 0){
+            for (int i = 0; i < ids_filmes_local.size(); i++) {
+                if(id_filme == ids_filmes_local.get(i)){
+                    ids_filmes_local.remove(i);
+                }
+            }
+            editor.clear();
+            if (editor.commit()){
+                salvarListaFilmes(ids_filmes_local);
+            }
+        }
+    }
+
+    public void salvarListaFilmes(List<Integer> ids_filmes){
+        //if(ids_filmes.size() > 0){
+            for (int i = 1; i <= ids_filmes.size(); i++) {
+                String chave_filme = "filme_" + i;
+                editor.putInt(chave_filme, ids_filmes.get(i-1));
+            }
+            editor.apply();
+        //}else{
+            //rv_meus_filmes.setVisibility(View.GONE);
+            //tv_sem_filmes.setVisibility(View.VISIBLE);
+       //}
+
+    }
+
+    public List<Integer> getIdsFilmes(){
+        List<Integer> ids_filmes = new ArrayList<>();
+        int qt_filmes = shared_preferences_filmes.getAll().size();
+        //Se lista de filmes estiver vazia uma mensagem será exibida
+        if (qt_filmes > 0){
+            for(int i = 1; i <= qt_filmes; i++){
+                String chave_filme = "filme_" + i;
+                ids_filmes.add(shared_preferences_filmes.getInt(chave_filme, 0));
+            }
+        }else {
+            return null;
+        }
+
+        //Retonará nulo para fazer tratamento no outro metodo
+        if (ids_filmes.size() > 0)
+            return ids_filmes;
+        else
+            return null;
+    }
+}
