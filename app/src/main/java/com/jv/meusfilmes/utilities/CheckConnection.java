b543dc4e2834d9898879babb6835d30f490e0dee
JoseVinicius-P <josevpf123@gmail.com>
2023-02-26 18:15:50 -0300

    fix: removed bug that caused connection warning to blink

--- app/src/main/java/com/jv/meusfilmes/utilities/CheckConnection.java
+++ app/src/main/java/com/jv/meusfilmes/utilities/CheckConnection.java
@@ -7,23 +7,29 @@ import android.os.Handler;
 
 public class CheckConnection {
 
+    private static Runnable runnable_local;
+    private static Handler handler;
     private static boolean is_internet = false;
 
     //Inicia um time que verificará se existe conexão com a internet
     public static void verificarInternet(Runnable runnable) {
+        removeRunnable();
         is_internet = false;
-        new Handler().postDelayed(runnable, 5000);
+        runnable_local = runnable;
+        handler = new Handler();
+        handler.postDelayed(runnable, 5000);
     }
 
     //Verificar se existe conexão ativa (WIFI, Dados móveis)
     public static boolean verificarConexao(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            return true;
-        } else {
-            return false;
-        }
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    private static void removeRunnable(){
+        if(handler != null && runnable_local != null)
+            handler.removeCallbacks(runnable_local);
     }
 
     public static void setIs_internet(boolean internet){
